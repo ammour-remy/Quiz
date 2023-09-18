bab525f0c9be6b6e4c2474eea238971fd3d9c183
ammour-remy <ammour.remy96@gmail.com>
2023-09-18 14:05:37 +0200

    up

--- src/partial/Header/Header.jsx
+++ src/partial/Header/Header.jsx
@@ -7,8 +7,9 @@ function Header(props) {
   
   return (
     <header id="navBar" className="d-flex  align-items-center justify-content-center">
+<article className="position-absolute start-0 ms-3">
 
-      <section className="mx-3 position-absolute start-0 ms-3">
+      <section className="mx-3">
         <a href="/Quiz/" title="lien vers la page d'accueil" alt='Logo chez switch'>
           <img src={logoRugby} id="logoRugby" alt="logo de de la world cup  france : 2023 de Rugby" />
         </a>
@@ -16,6 +17,7 @@ function Header(props) {
       <p className="size fw-semibold m-0">
         RUGBY <span className="fw-bold">WORLD CUP</span> FRANCE 2023
       </p>
+</article>
       <h2 className="">MYVINGO</h2>
       <button className={`position-absolute end-0 m-3 rounded-5 ${props.none === "none" ? "" : "d-none"}`} 
       onClick={() =>  props.setclosePopUp(false)}>
